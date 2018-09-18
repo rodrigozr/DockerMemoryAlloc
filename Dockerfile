ea75415f6686de30230248a1aee84bf1f1ed1272
rrosauro <rrosauro@rdisoftware.com>
2018-09-18 15:07:23 -0300

    First prototype

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM microsoft/dotnet-framework:3.5-runtime-20180508-windowsservercore-ltsc2016
+COPY alloc\bin\x86\Debug\alloc.exe c:/
---
+++ alloc/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alloc
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var totalMemStr = args.Length > 0 ? args[0] : "1.3 GB";
+            var blockSizeStr = args.Length > 1 ? args[1] : "64 MB";
+            var totalMem = Parse(totalMemStr);
+            var blockSize = Parse(blockSizeStr);
+            Console.WriteLine($"Allocating {totalMemStr} in blocks of {blockSizeStr}...");
+            var blocks = new List<byte[]>();
+            long totalAllocated = 0;
+            while (totalAllocated < totalMem)
+            {
+                var thisBlockSize = Math.Min(blockSize, totalMem - totalAllocated);
+                try
+                {
+                    blocks.Add(new byte[thisBlockSize]);
+                    totalAllocated += thisBlockSize;
+                }
+                catch (OutOfMemoryException)
+                {
+                    Console.WriteLine($"FAILED - Could allocate only {totalAllocated} out of {totalMem}. Failed block size: {thisBlockSize}");
+                    return 1;
+                }
+            }
+
+            blocks.Clear();
+            Console.WriteLine("SUCCESS");
+            return 0;
+        }
+
+        private static long Parse(string str)
+        {
+            const int KB = 1024;
+            const int MB = KB * 1024;
+            const int GB = MB * 1024;
+            try
+            {
+                var split = str.Split(' ');
+                var bytes = decimal.Parse(split[0], null);
+                switch (split[1].ToLowerInvariant())
+                {
+                    case "kb": bytes *= KB; break;
+                    case "mb": bytes *= MB; break;
+                    case "gb": bytes *= GB; break;
+                }
+                return (long) bytes;
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Invalid input: {str}", e);
+            }
+        }
+    }
+}
